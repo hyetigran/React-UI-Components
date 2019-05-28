4abf64e41e52921a580ceca28810f60df6f4728f
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-05-28 11:03:56 +0400

    complete clickHandler

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -4,11 +4,8 @@ import CardContent from './CardContent';
 import CardBanner from './CardBanner';
 
 class CardContainer extends Component {
-	state = {
-		link: 'https://www.reactjs.org'
-	};
 	clickHandler = () => {
-		return <a href={this.state.link} />;
+		return (window.location.href = 'https://reactjs.org');
 	};
 	render() {
 		return (
