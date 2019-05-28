826e529ad51bb48f9c228606b0b84bbf8efc8eaf
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-05-28 18:52:18 +0400

    add numbers component

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,6 @@
 import React from 'react';
 import './Button.css';
+
+const actionButton = props => <button className="Button">{props.label}</button>;
+
+export default actionButton;
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Display.css';
+
+const calculatorDisplay = props => {
+	return (
+		<div className="Display">
+			<output name="displayValue" value={props.value} />
+		</div>
+	);
+};
+
+export default calculatorDisplay;
