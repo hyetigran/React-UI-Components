91d56cd5c2821999b7a9f704d96b34c5c9549f30
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-05-29 10:56:40 +0400

    clean up files

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ButtonContainer.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import './Button.css';
-
-const buttonContainer = props => {
-	return <div className={`ButtonContainer ${props.className}`}>{props.children}</div>;
-};
-export default buttonContainer;
